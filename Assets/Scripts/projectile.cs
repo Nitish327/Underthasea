6b236ec21a1897cc10ea7a1343a4ca1aa20fd43e
Nitish327 <nrrsoccer@gmail.com>
2026-02-28 19:25:38 -0600

    bows are done

--- Assets/Scripts/projectile.cs
+++ Assets/Scripts/projectile.cs
@@ -5,35 +5,45 @@ public class Projectile : MonoBehaviour
 {
     public GameObject arrow;
     public Transform arrowSpawn;
-    public int ammo;
-    public Text ammoText;
+    public int ammo = 10;
+    public Text ammoText; // Legacy UI Text
     public Animator bowAnim;
     public string shootAnimationName;
     public AudioSource bowSound;
 
     public DisableArmsEnableGun arms;
 
-  
-
+    void Start()
+    {
+        UpdateAmmoUI();
+    }
 
     void Update()
     {
-        ammoText.text = ammo.ToString();
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             if (ammo > 0 && arms.showArms == false)
             {
-                bowAnim.Play(shootAnimationName);
-                Instantiate(arrow, arrowSpawn.position, arrowSpawn.rotation);
-                bowSound.Play();
-                ammo--;
-            
+                ShootArrow();
             }
         }
     }
 
+    void ShootArrow()
+    {
+        ammo--;
+        bowAnim.Play(shootAnimationName);
+        Instantiate(arrow, arrowSpawn.position, arrowSpawn.rotation);
+        UpdateAmmoUI();
+        Debug.Log("Ammo: " + ammo);
+        bowSound.Play();
+    }
 
-
-
-
+    void UpdateAmmoUI()
+    {
+        if (ammoText != null)
+            ammoText.text = ammo.ToString();
+        else
+            Debug.LogWarning("Ammo Text is not assigned in the Inspector!");
+    }
 }
